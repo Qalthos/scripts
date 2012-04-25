2fa8333b6f933ffd5ea15e45f1608998abd79fc8
Nathaniel Case <Qalthos@gmail.com>
2012-04-25 15:40:01 -0400

    Intent: look for *-release file, then lsb, ifa available, then fallback to uname

--- distro_check.sh
+++ distro_check.sh
@@ -5,22 +5,27 @@ then
     echo "Gentoo"
 
 else
-    uname=$(uname -r)
-    case $uname in
-        *ARCH)
-            echo "Arch"
-            ;;
-        *-amd64 | *-686-pae)
-            echo "Debian"
-            ;;
-        *-generic)
-            echo "Ubuntu"
-            ;;
-        *x86_64)
-            echo "Fedora"
+    lsb=$(lsb_release -si)
+    case $lsb in
+        Ubuntu)
+            echo $lsb
             ;;
         *)
-            echo $uname
+            uname=$(uname -r)
+            case $uname in
+                *ARCH)
+                    echo "Arch"
+                    ;;
+                *-amd64 | *-686-pae)
+                    echo "Debian"
+                    ;;
+                *x86_64)
+                    echo "Fedora"
+                    ;;
+                *)
+                    echo $uname
+                    ;;
+            esac
             ;;
     esac
 fi
